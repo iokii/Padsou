59116d7845b1f334bf030fd3a19c525239330fbd
ioki <thomas.boyer.t2@gmail.com>
2023-01-23 10:36:51 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/padsou/MainActivity.kt
+++ app/src/main/java/com/example/padsou/MainActivity.kt
@@ -4,30 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
+<<<<<<< Updated upstream
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.foundation.layout.Column
+=======
+>>>>>>> Stashed changes
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+<<<<<<< Updated upstream
 
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -40,33 +32,10 @@ import coil.compose.AsyncImage
 import com.example.padsou.models.User
 
 
+=======
+>>>>>>> Stashed changes
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.padsou.models.Comment
-import com.example.padsou.models.Filter
-import com.example.padsou.models.Offer
-import com.example.padsou.models.User
-import com.example.padsou.ui.components.*
-
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.padsou.ui.components.*
-import com.example.padsou.ui.components.Register
-import com.example.padsou.ui.AddPlanDesc
-import com.example.padsou.ui.AddPlanPhoto
-import com.example.padsou.ui.MainNav
-import com.example.padsou.ui.theme.DarkBlue
-import com.example.padsou.ui.theme.GrayWhite
-
 import com.example.padsou.ui.theme.PadsouTheme
 
 class MainActivity : ComponentActivity() {
@@ -77,10 +46,8 @@ class MainActivity : ComponentActivity() {
             PadsouTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-
                     modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
-
-                    color = DarkBlue
+                    color = Color(0xFF5F67EA)
 
                 ) {
 
@@ -89,6 +56,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
 
 
